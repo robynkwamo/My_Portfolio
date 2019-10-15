ad85cd7a81bbac19e26b594f25dcd0b389d098ac
robynkwamo <48221148+robynkwamo@users.noreply.github.com>
2019-10-15 01:57:27 -0500

    Put the previous picture back

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -8,7 +8,7 @@ class Landing extends Component {
                 <Grid className = "landing-grid">
                     <Cell col={12}>
                         <img 
-                            src= "https://r7ctow.dm.files.1drv.com/y4m7OrUyGv6TGW4meP_xgxpFAGP1kdbQUf_L45q30NThT65Gm9vM0FrTrOyO9yFxmQnQG7Pceoyqea5SXmc90pJJpz6IehRtR9XrOdkH86UjUjnMOGu7WXYlINxHGv5Jb1c7aC6vNH1CPI0s7RW2YpMT6CDOZAlv_gqk6gadYBsZVtxU1_e-X6Dv6fWvuKYGaSxwZ5UyernpI6AJd-h4KBk4g?width=917&height=1529&cropmode=none" 
+                            src= "https://fdwzow.dm.files.1drv.com/y4m051q3R3P-xF2TcEQgdSR8rhhd4gCtW9ysTJIBl850-fCWK9DHpvaLDrQQok2t15FJNn_oQlk0xpxuVw7_xkhSr7FmmSLd8RB3lcazu5zqhxx0HEVI0Npc4UoFYWbbvNGcpltOvTgr1FFEFKs1mlQXFLyPm6IX4vEDaI7DDaSE0GHcJT3wiURJhOrh5vFqB9iK_L641ikUqe5XaPEBzfGAQ?width=1350&height=650&cropmode=none" 
                             alt = "robypic"
                             className = "profil_picture"
                         />
